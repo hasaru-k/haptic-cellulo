51b761ee67211be4fac36ada91776c3be6f1429e
hasaru-k <hasaru-k@users.noreply.github.com>
2021-03-18 22:02:59 +1100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+let url = "http://cellulo-live.herokuapp.com/pose";
 
 function App() {
+
+  const [poses, setPoses] = useState('');
+
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    setPoses(JSON.stringify(data));
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p>
+        {poses}
+      </p>
     </div>
   );
 }
